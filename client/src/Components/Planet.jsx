2d73f2982df4a4577434561c9f8fe0057e9db47f
ashuOO7 <ashjnavj@gmail.com>
2025-09-29 23:17:03 +0530

    hover effect in action

--- client/src/Components/Planet.jsx
+++ client/src/Components/Planet.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { gql } from "@apollo/client";
 import { useQuery } from "@apollo/client/react";
-
+import HoverVideoPlayer from 'react-hover-video-player';
 
 const QUERY_ALL_PLANETS = gql`
   query GetAllPlanets {
@@ -9,6 +9,7 @@ const QUERY_ALL_PLANETS = gql`
       id
       name
       image
+      planets_bg
       details {
         about
       }
@@ -51,26 +52,25 @@ export default function Planet() {
   }
 
   return (
-    <div className="flex justify-center items-center min-h-screen w-full bg-no-repeat bg-center bg-cover sm:bg-fixed px-4 sm:px-6 md:px-8 cursor-none p-20"
-      style={{ backgroundImage: `url(/planet_bg/${planet.image})` }}>
-      <div
-        name="card_1"
-        className="w-full sm:w-11/12 md:w-3/4 min-h-[60vh] md:min-h-[75vh] 
-               p-4 sm:p-6 md:p-8 border-4 sm:border-6 md:border-8 
-               border-gray-800 rounded-lg flex flex-col items-center 
-               justify-center space-y-4 sm:space-y-6"
-      >
+    <div className="flex justify-center items-center min-h-screen w-full bg-no-repeat bg-center bg-cover sm:bg-fixed px-4 sm:px-6 md:px-8 cursor-none p-20">
+    <div className="absolute inset-0 flex justify-center items-center z-0">
+      <HoverVideoPlayer 
+        videoSrc={`/planets_bg/${planet.planets_bg}`} 
+        loadingOverlay={
+          <div className="w-full h-full flex items-center justify-center bg-gray-900 text-white rounded-lg">
+            Loading...
+          </div>
+        }
+      />
+    </div>
+      <div name="card_1" className="w-full sm:w-11/12 md:w-3/4 min-h-[60vh] md:min-h-[75vh] p-4 sm:p-6 md:p-8 border-4 sm:border-6 md:border-8 border-gray-800 rounded-lg flex flex-col items-center justify-center space-y-4 sm:space-y-6">
         {/* Planet Title */}
-        <p className="font-serif text-white 
-                 text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center">
+        <p className="font-serif text-white z-1 text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center">
           {planet.name.split("").join(" ")}
         </p>
 
         {/* Nested Card 2 */}
-        <div
-          name="card_2"
-          className="w-full p-4 sm:p-6 border border-gray-950 
-                 rounded-lg bg-white/5 backdrop-blur-sm" >
+        <div name="card_2" className="w-full p-4 sm:p-6 border border-gray-950 rounded-lg bg-white/5 backdrop-blur-sm" >
           <p className="font-serif text-gray-300 text-sm sm:text-base md:text-lg text-center leading-relaxed">
             {planet.details?.about || "No details available"}
           </p>
